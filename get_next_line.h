0935e10942ed47c961f7a7a8a4b15decea88d257
Jerome DESMARES <jdesmare@e1r7p12.42.fr>
2016-12-14 18:16:50 +0100

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdesmare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/14 16:19:32 by jdesmare          #+#    #+#             */
+/*   Updated: 2016/11/28 18:07:50 by jdesmare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include "./libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
